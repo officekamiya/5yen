2cab095bbcfc292a0fc7110d3a7f124758ea6282
Kamiya Hayato <hayato@officekamiya.co.jp>
2020-01-13 00:46:14 +0900

    Change stable sound

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from hx711 import hx711
 from kivy.config import Config
 Config.set('graphics', 'width', '640')
 Config.set('graphics', 'height', '400')
-from kivy.core.window import Window
-Window.fullscreen = True
+#from kivy.core.window import Window
+#Window.fullscreen = True
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -45,64 +45,88 @@ print("Tare done! Add weight now...")
 
 #Like the common value
 class CV():
-    flagloop = True
-    flagtare = False
-    weight = 0
-    listweight = [0]
+    loopflag = True
+    tareflag = False
+    stableflag = False
+
+    dispweight = 0
+    currweight = 0
+    prevweight = 0
+    saveweight = [0]
 
 def SoundTare():
     os.system("aplay --quiet './sounds/Hard_Tech_Bass_A4.wav'")
 
-def WeightGet():
-    CV.listweight.append(hx.get_weight(7))
+def SoundStable():
+    if CV.stableflag == True:
+        os.system("aplay --quiet './sounds/Hard_Tech_Bass_A4.wav'")
+        print("sound play")
+        CV.stableflag = False
+
+
+def GetWeight():
+    CV.saveweight.append(hx.get_weight(7))
     hx.reset()
-    time.sleep(0.01)
+    time.sleep(0.1)
+    print(CV.saveweight)
+    if len(CV.saveweight) > 5:
+        del CV.saveweight[0]
 
 #Get the weight from HX711
-def WeightCalc():
-    CV.flagloop = True
+def CalcWeight():
+    CV.loopflag = True
 
-    while CV.flagloop:
+    while CV.loopflag:
 
-        if CV.flagtare == True:
+        if CV.tareflag == True:
             hx.reset()
             hx.tare(39)
-            del CV.listweight[0:]
-            CV.weight = 0
-            CV.flagtare = False
+            del CV.saveweight[0:]
+            CV.dispweight = 0
+            CV.tareflag = False
             print("tared")
 
         else:
-            if len(CV.listweight) <= 10:
-                WeightGet()
+            GetWeight()
+            CV.currweight = sum(CV.saveweight) / len(CV.saveweight)
+            print(CV.currweight)
+
+            if CV.currweight < 2:
+                executor.submit(SoundStable)
+                CV.dispweight = 0
+                print("around 0")
+
+            elif CV.currweight < CV.prevweight + 2 and CV.currweight > CV.prevweight - 2:
+                executor.submit(SoundStable)
+                CV.dispweight = CV.prevweight
+                print("stable")
 
             else:
-                del CV.listweight[0]
-                WeightGet()
-                CV.weight = sum(CV.listweight) / len(CV.listweight)
-
-                print(CV.weight)
+                CV.dispweight = CV.currweight
+                CV.prevweight = CV.currweight
+                print("movable")
+                CV.stableflag = True
 
     print("Cleaning and Exit")
     GPIO.cleanup()
     sys.exit()
 
 class LayoutAdd(BoxLayout):
-    weight = NumericProperty(0)
+    dispweight = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super(LayoutAdd, self).__init__(**kwargs)
         Clock.schedule_interval(self.LabelWeight, 0.1)
 
     def LabelWeight(self, dt):
-        self.weight = CV.weight
+        self.dispweight = CV.dispweight
 
-    def ButtonTare(self):
-        CV.flagtare = True
+    def TareButton(self):
+        CV.tareflag = True
         executor.submit(SoundTare)
 
-    def ButtonExit(self):
-        CV.flagloop = False
+    def ExitButton(self):
+        CV.loopflag = False
         App.get_running_app().stop()
 
 class MainApp(App):
@@ -115,5 +139,5 @@ class MainApp(App):
 
 if __name__ == '__main__':
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-    executor.submit(WeightCalc)
+    executor.submit(CalcWeight)
     MainApp().run()
